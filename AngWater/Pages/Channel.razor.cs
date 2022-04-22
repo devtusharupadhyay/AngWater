b3dbd223a9a8d52b7cc94610d4d989abe6388923
Tushar Upadhyay <dev@tusharupadhyay.com>
2022-04-22 22:08:30 +0530

    new design changes

--- AngWater/Pages/Channel.razor.cs
+++ AngWater/Pages/Channel.razor.cs
@@ -24,26 +24,34 @@ namespace AngWater.Pages
                 if (_id == 2)
                 {
                     ActiveChannel = Tabs.Channel2;
-                    ChannelURI = "https://video.ibm.com/embed/24088760?autoplay=true";
+                    ChannelURI = "https://video.ibm.com/embed/23750985?autoplay=true";
                 }
                 if (_id == 3)
                 {
                     ActiveChannel = Tabs.Channel3;
-                    ChannelURI = "https://video.ibm.com/embed/24088760?autoplay=true";
+                    ChannelURI = "https://video.ibm.com/embed/24088761?autoplay=true";
                 }
                 if (_id == 4)
                 {
                     ActiveChannel = Tabs.Channel4;
-                    ChannelURI = "https://video.ibm.com/embed/24088760?autoplay=true";
+                    ChannelURI = "https://video.ibm.com/embed/24066306?autoplay=true";
                 }
 
                 StateHasChanged();
             }
         }
 
+        public int BottomChannelId { get; set; }
+
         public string ChannelURI { get; set; }
 
         public Tabs ActiveChannel { get; set; }
+
+        private void BottomChannelClicked(int bottomChannel)
+        {
+            BottomChannelId = bottomChannel;
+            StateHasChanged();
+        }
     }
 
     public enum Tabs
